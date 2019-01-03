8f2cceb1918bebb8e1ea01fa4436b01a31f3a30b
Caleb <calebgryfe@gmail.com>
2019-01-03 13:50:16 -0500

    Update LocalStorage.java

--- mobileApplication/LocalStorage.java
+++ mobileApplication/LocalStorage.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 /**
  * Created by steve on 11/6/2017.
- * Steven Nash | 100973959
+ * Steven Nash 
  * LocalStorage:
  *      This class was created as a temporary save location for values used globally in the app.
  *      This data is erased upon the closing of the application.
