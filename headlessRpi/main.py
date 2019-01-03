b00b693bd5423beb38a300a019088cf531040111
Caleb <calebgryfe@gmail.com>
2019-01-03 13:55:04 -0500

    Update main.py

--- headlessRpi/main.py
+++ headlessRpi/main.py
@@ -1,8 +1,8 @@
 '''
 Created on Nov 25, 2017
 
-@author: Muhammad Tarequzzaman |100954008| 
-@Co-Author Caleb Gryfe |101009798|
+@author: Muhammad Tarequzzaman 
+@Co-Author Caleb Gryfe 
 '''
 
 import StepperMotor as st
