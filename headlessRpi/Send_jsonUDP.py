b9225877bc0f96f74d3e50bdee5f8e600577275c
Caleb <calebgryfe@gmail.com>
2019-01-03 13:53:26 -0500

    Update Send_jsonUDP.py

--- headlessRpi/Send_jsonUDP.py
+++ headlessRpi/Send_jsonUDP.py
@@ -2,8 +2,7 @@ import socket
 import Read_serial as S
 import requests as R
 
-@author: Muhammad Tarequzzaman
-@co-author: Caleb Gryfe 
+
     
 #===============================================================================
 # # this class have the codes for sending data over UDP for specific address and port. 
